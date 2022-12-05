7bb5f81f79de85c97ab534ec3e3340ee864a3e8d
Bernhard Schmitt <schmitt@sitegeist.de>
2022-12-05 11:00:06 +0100

    WIP: Implement dummy geocoder

---
+++ Classes/Domain/YellowMapsGeoCoder.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This script belongs to the Neos Flow package "Sitegeist.GeographicLibrary.YellowMapsAdapter".
+ */
+
+declare(strict_types=1);
+
+namespace Sitegeist\GeographicLibrary\YellowMapsAdapter\Domain;
+
+use Nezaniel\GeographicLibrary\Application\Value\GeoCoordinates;
+use Nezaniel\GeographicLibrary\Domain\Repository\GeoCoderInterface;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\I18n\Service as LocalizationService;
+
+#[Flow\Scope('singleton')]
+class YellowMapsGeoCoder implements GeoCoderInterface
+{
+    public function __construct(
+        private readonly LocalizationService $localizationService,
+        private readonly string $apiKey
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetchCoordinatesByAddress(string $address): GeoCoordinates
+    {
+        return new GeoCoordinates(0, 0);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetchCoordinatesByPostalCode(string $zip, string $countryCode): GeoCoordinates
+    {
+        return new GeoCoordinates(0, 0);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function enrichGeoCoordinates(GeoCoordinates $coordinates): GeoCoordinates
+    {
+        return new GeoCoordinates(0, 0);
+    }
+}
